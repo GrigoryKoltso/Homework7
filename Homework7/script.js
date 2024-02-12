5d3d271396b5f64c1970688f4fb480abc1631329
GrigoryKoltso <154324680+GrigoryKoltso@users.noreply.github.com>
2024-02-12 08:05:37 +0100

    Add files via upload

---
+++ Homework7/script.js
@@ -0,0 +1,78 @@
+/* num1 = 21;
+num2 = 16;
+if(num1 < num2) {
+    console.log(num1);
+}else if(num1 > num2) {
+    console.log(num2);
+}else {
+    console.log();
+} */
+
+
+
+
+/* function even(a, b) {
+    if (a > b) {
+        for (let i = a; i > b; i--) {
+            if (i % 2 === 0) {
+                console.log(i)
+            }
+
+        }
+    } else if (a < b) {
+        for (let i = b; i > a; i--) {
+            if (i % 2 === 0) {
+                console.log(i)
+            }
+
+        }
+    }
+}
+
+even(48,16) */
+
+
+
+
+
+/* function power(a,b) {
+    b = 2;
+    console.log(a ** b);
+}
+power(4) */
+
+
+
+
+/* let summa = 0;
+function counter(n) {
+    for(let i = 1; i < n; i++) {
+        summa += i;
+        
+    }
+    console.log(summa);
+}
+counter(10) */
+
+
+
+
+/* let chet = 0;
+let nechet = 0;
+function numbers(n,m) {
+    for(let i = n; i < m; i++) {
+        if(i %2 === 0) {
+            chet += i;
+        }
+         else if(i %2 != 0) {
+            nechet += i;
+        }
+    }console.log(chet)
+    console.log(nechet)
+}
+numbers(10,16) */
+
+
+
+
+
